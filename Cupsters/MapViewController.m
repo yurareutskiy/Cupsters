125bb844eac42bc4ab84cde4672b70310e2f8864
Anton <avscherbakov@icloud.com>
2016-01-16 22:26:11 +0300

    Google Maps.Part I.
    Continued…

--- Cupsters/MapViewController.m
+++ Cupsters/MapViewController.m
@@ -8,13 +8,22 @@
 
 #import "MapViewController.h"
 @import GoogleMaps;
+#import "Constants.h"
+#import "UIColor+HEX.h"
+#import "PlacePhotoTableViewCell.h"
+#import "MenuRevealViewController.h"
+#import "SWRevealViewController.h"
 
 @interface MapViewController ()
+
+@property (strong, nonatomic) MenuRevealViewController *menu;
+@property (strong, nonatomic) UIBarButtonItem *menuButton;
+@property (strong, nonatomic) SWRevealViewController *reveal;
+@property (strong, nonatomic) IBOutlet GMSMapView *mapView_;
 
 @end
 
 @implementation MapViewController {
-    GMSMapView *mapView_;
     CLLocationManager *locationManager;
 }
 
@@ -28,25 +37,92 @@
     locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     [locationManager requestWhenInUseAuthorization];
     [locationManager startUpdatingLocation];
+
+    GMSCameraPosition *camera = [GMSCameraPosition
+                                 cameraWithLatitude:locationManager.location.coordinate.latitude
+                                 longitude:locationManager.location.coordinate.longitude
+                                 zoom:15];
     
-    // Create a GMSCameraPosition that tells the map to display the
-    // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
-    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-    mapView_.myLocationEnabled = YES;
-    self.view = mapView_;
+    _mapView_ = [GMSMapView mapWithFrame:CGRectMake(_mapView_.frame.origin.x, _mapView_.frame.origin.y, _mapView_.frame.size.width, _mapView_.frame.size.height) camera:camera];
+    _mapView_.myLocationEnabled = YES;
+    self.view = _mapView_;
     
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
     marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
     marker.title = @"Sydney";
     marker.snippet = @"Australia";
-    marker.map = mapView_;
-    mapView_.settings.myLocationButton = YES;
+    marker.map = _mapView_;
+    _mapView_.settings.myLocationButton = YES;
+    
+    [self setNeedsStatusBarAppearanceUpdate];
+    [self customNavBar];
+    [self preferredStatusBarStyle];
+    [self configureMenu];
     
     // Do any additional setup after loading the view.
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    self.menu.view.frame = CGRectMake(self.menu.view.frame.origin.x, 0.f, 280.f, self.menu.view.frame.size.height + 60.f);
+}
+
+- (void)customNavBar {
+    
+    // Set background color
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithHEX:cBrown];
+    self.navigationController.navigationBar.translucent = NO;
+    
+    
+    // Set title view
+    
+    self.navigationItem.titleView = [self customTitleViewWithImage];
+    
+}
+
+- (void)configureMenu {
+    
+    self.reveal = self.revealViewController;
+    
+    if (!self.reveal) {
+        return;
+    }
+    
+    
+    // Add gesture recognizer
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    
+    // Set menu button
+    self.menuButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menu"]
+                                                       style:UIBarButtonItemStyleDone
+                                                      target:self.revealViewController
+                                                      action:@selector(revealToggle:)];
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationItem.leftBarButtonItem = self.menuButton;
+    
+}
+
+- (UILabel*)customTitleViewWithImage {
+    
+    // Create
+    UILabel* navigationBarLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 150, 30)];
+    
+    // Create text attachment, which will contain and set text and image
+    NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
+    attachment.image = [UIImage imageNamed:@"cup"];
+    NSAttributedString *attachmentString = [NSAttributedString attributedStringWithAttachment:attachment];
+    NSMutableAttributedString *myString= [[NSMutableAttributedString alloc] initWithString:@"1 ЧАШКА  "];
+    [myString appendAttributedString:attachmentString];
+    
+    // Configure our label
+    navigationBarLabel.attributedText = myString;
+    navigationBarLabel.font = [UIFont fontWithName:cFontMyraid size:18.f];
+    navigationBarLabel.textColor = [UIColor whiteColor];
+    navigationBarLabel.textAlignment = NSTextAlignmentCenter;
+    
+    return navigationBarLabel;
 }
 
 - (void)didReceiveMemoryWarning {
