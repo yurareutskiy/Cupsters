dbe672f127b0c0e2f6530decbba98f041bb56a26
Anton <avscherbakov@icloud.com>
2016-01-17 21:19:59 +0300

    A lot of new views
    Go-go-go

---
+++ Cupsters/TariffViewController.h
@@ -0,0 +1,13 @@
+//
+//  TariffViewController.h
+//  Cupsters
+//
+//  Created by Anton Scherbakov on 17/01/16.
+//  Copyright © 2016 Styleru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TariffViewController : UIViewController
+
+@end
---
+++ Cupsters/TariffViewController.m
@@ -0,0 +1,37 @@
+//
+//  TariffViewController.m
+//  Cupsters
+//
+//  Created by Anton Scherbakov on 17/01/16.
+//  Copyright © 2016 Styleru. All rights reserved.
+//
+
+#import "TariffViewController.h"
+
+@interface TariffViewController ()
+
+@end
+
+@implementation TariffViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
