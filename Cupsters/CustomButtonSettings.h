9f73c2fed20cee204fe4ccf1db2635fec1408534
Anton <avscherbakov@icloud.com>
2016-01-18 13:20:46 +0300

    IBDesignable is better + TariffViewController
    Ebashim :3

--- Cupsters/CustomButtonSettings.h
+++ Cupsters/CustomButtonSettings.h
@@ -9,7 +9,14 @@
 #import <UIKit/UIKit.h>
 IB_DESIGNABLE
 @interface CustomButtonSettings : UIButton
-@property (nonatomic) IBInspectable NSInteger lineWidth;
-@property (nonatomic) IBInspectable UIColor *fillColor;
+@property (nonatomic) IBInspectable CGFloat cornerRadius;
+@property (nonatomic) IBInspectable CGFloat borderWidth;
+@property (nonatomic) IBInspectable CGFloat shadowRadius;
+@property (nonatomic) IBInspectable CGFloat widthOffset;
+@property (nonatomic) IBInspectable CGFloat heightOffset;
+@property (nonatomic) IBInspectable UIColor *shadowColor;
+@property (nonatomic) IBInspectable BOOL masksToBounds;
+@property (nonatomic) IBInspectable float shadowOpacity;
+
 
 @end
--- Cupsters/CustomButtonSettings.m
+++ Cupsters/CustomButtonSettings.m
@@ -11,12 +11,20 @@
 @implementation CustomButtonSettings
 
 - (void)drawRect:(CGRect)rect {
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGRect myFrame = self.bounds;
-    CGContextSetLineWidth(context, _lineWidth);
-    CGRectInset(myFrame, 5, 5);
-    [_fillColor set];
-    UIRectFrame(myFrame);
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGRect myFrame = self.bounds;
+//    CGContextSetLineWidth(context, _borderWidth);
+//    CGRectInset(myFrame, 5, 5);
+//    UIRectFrame(myFrame);
+    
+    self.layer.cornerRadius = _cornerRadius;
+    self.layer.masksToBounds = _masksToBounds;
+    self.layer.borderWidth = _borderWidth;
+    self.layer.shadowColor = _shadowColor.CGColor;
+    self.layer.shadowOpacity = _shadowOpacity;
+    self.layer.shadowRadius = _shadowRadius;
+    self.layer.shadowOffset = CGSizeMake(_widthOffset, _heightOffset);
+
 }
 
 @end
