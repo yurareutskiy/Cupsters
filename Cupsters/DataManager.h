e8eebe1539e9b95bb9e003d82dbe7e7498b6a8c0
yurareutskiy <yurareutskiy@gmail.com>
2016-01-22 20:33:40 +0300

    Login VC fix issues

---
+++ Cupsters/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  Cupsters
+//
+//  Created by Reutskiy Jury on 1/22/16.
+//  Copyright © 2016 Styleru. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface DataManager : NSFetchedResultsController
+
+@end
---
+++ Cupsters/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  Cupsters
+//
+//  Created by Reutskiy Jury on 1/22/16.
+//  Copyright © 2016 Styleru. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
